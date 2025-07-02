9d2190d90e417d6bd10c5261d5bb62aec27eccd9
adriiberjaga <adriabdd@gmail.com>
2025-07-02 12:15:46 +0200

    .

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,13 +1,14 @@
-import { useState, type ReactNode } from 'react';
-import { AnimatePresence, motion } from 'motion/react';
-import { twMerge } from 'tailwind-merge';
+import { useState, type ReactNode } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   children: ReactNode;
   className?: string;
   timeout?: number;
-  type?: 'error' | 'warning' | 'info';
+  type?: "error" | "warning" | "info";
 }
+//
 
 export default function Alert(props: Props) {
   const { children, className, timeout, type } = props;
@@ -19,10 +20,10 @@ export default function Alert(props: Props) {
   }, timeout || 3000);
 
   const classes = twMerge(
-    'fixed top-2 right-2 z-50 bg-red-500 text-white p-4 rounded-lg shadow-md',
-    type === 'error' && 'bg-red-500',
-    type === 'warning' && 'bg-yellow-500',
-    type === 'info' && 'bg-blue-500',
+    "fixed top-2 right-2 z-50 bg-red-500 text-white p-4 rounded-lg shadow-md",
+    type === "error" && "bg-red-500",
+    type === "warning" && "bg-yellow-500",
+    type === "info" && "bg-blue-500",
     className
   );
 
@@ -30,9 +31,9 @@ export default function Alert(props: Props) {
     <AnimatePresence>
       {isVisible && (
         <motion.div
-          initial={{ x: '100%' }}
+          initial={{ x: "100%" }}
           animate={{ x: 0 }}
-          exit={{ x: '100%' }}
+          exit={{ x: "100%" }}
           className={classes}
         >
           {children}
